c1addba45ec856c966ff914c6dc982fabe71087c
Jack <jkotheimer9@gmail.com>
2024-12-21 22:51:02 -0600

    Updated apis

--- src/api/v1/users/index.ts
+++ src/api/v1/users/index.ts
@@ -14,6 +14,26 @@ function getUserById(request: Request, response: Response) {
     });
 }
 
+/**
+ * @description Create a user record.
+ * Inputs:
+ * - Valid CSRF token
+ * - Valid ReCaptcha token
+ * - JSON User payload
+ * Logic:
+ * - Validate CSRF token
+ * - Validate ReCaptcha token
+ * - Ensure all required fields and no extra fields are present
+ * - Ensure unique email
+ * - Ensure secure password
+ * - Create User record
+ * Outputs:
+ * - Record Id
+ * - Auth Token
+ * - CSRF Token
+ * @param request
+ * @param response
+ */
 function createUser(request: Request, response: Response) {
     const payload = request.body;
     console.log('CREATE USER', payload);
@@ -22,7 +42,36 @@ function createUser(request: Request, response: Response) {
     });
 }
 
-router.get('/:id', getUserById);
+/**
+ * @description Create a user record.
+ * Inputs:
+ * - Valid CSRF token
+ * - Valid ReCaptcha token
+ * - JSON User payload (diff)
+ * Logic:
+ * - Validate CSRF token
+ * - Validate ReCaptcha token
+ * - Ensure only valid fields are present in payload
+ * - Ensure unique email
+ * - Ensure secure password
+ * - Create User record
+ * Outputs:
+ * - Record Id
+ * - Auth Token
+ * - CSRF Token
+ * @param request
+ * @param response
+ */
+function updateUser(request: Request, response: Response) {
+    const payload = request.body;
+    console.log('UPDATE USER', payload);
+    response.status(200).json({
+        message: 'Success'
+    });
+}
+
 router.post('/', createUser);
+router.get('/:id', getUserById);
+router.patch('/:id', updateUser);
 
 export default router;
--- src/database/database.ts
+++ src/database/database.ts
@@ -2,9 +2,24 @@ import mysql from 'mysql';
 
 export default class Database {
     public static connection: mysql.Connection;
+
+    /**
+     * Static initializer to initialize database connection
+     */
     static {
-        const connection = mysql.createConnection({});
+        Database.connection = mysql.createConnection({
+            host: process.env.MYSQL_HOST,
+            database: process.env.MYSQL_DATABASE,
+            user: process.env.MYSQL_USER,
+            password: process.env.MYSQL_PASSWORD
+        });
     }
+
+    /**
+     * @description Execute a SQL query
+     * @param query SQL query string
+     * @returns Promise that resolves to an array of records resulting from query
+     */
     public static async query(query: string): Promise<Array<Record<string, any>>> {
         return new Promise<Array<Record<string, any>>>((resolve, reject) => {
             Database.connection.query(query, (error, results, fields) => {
