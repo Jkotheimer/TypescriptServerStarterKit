e1c4f91fdc48987c3abf2aa81b14bea52d2c715f
Jack <jkotheimer9@gmail.com>
2024-12-29 11:27:33 -0600

    Added string utils module for formatting strings

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,7 +1,14 @@
 export default class StringUtils {
     public static format(formatString: string, variables: string[]): string {
         return formatString.replace(/{(\d+)}/g, function (match, index) {
-            return typeof variables[index] != 'undefined' && typeof index === 'number' ? variables[index] : match;
+            if (typeof index !== 'number') {
+                index = parseInt(index);
+            }
+            return typeof variables[index] != 'undefined' ? variables[index] : match;
         });
+    }
+
+    public static escapeSingleQuotes(str: string): string {
+        return str.replace(/'/g, "\\'");
     }
 }
