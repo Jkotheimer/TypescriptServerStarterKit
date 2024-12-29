33d8838a5743292d089a71d4af9a9cf4c9bb988b
Jack <jkotheimer9@gmail.com>
2024-12-29 11:27:49 -0600

    Filled out crud operations

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,4 +1,7 @@
+import { DatabaseError } from '@models/errors';
+import BaseModel from '@models/base';
 import mysql from 'mysql';
+import Constants from '@constants';
 
 export default class Database {
     public static connection: mysql.Connection;
@@ -20,16 +23,107 @@ export default class Database {
      * @param query SQL query string
      * @returns Promise that resolves to an array of records resulting from query
      */
-    public static async query(query: string): Promise<Array<Record<string, any>>> {
+    public static async query(query: string, variables: any[] = []): Promise<Array<Record<string, any>>> {
         return new Promise<Array<Record<string, any>>>((resolve, reject) => {
-            Database.connection.query(query, (error, results, fields) => {
-                console.log(fields);
-                console.log(results);
+            const formattedQuery = Database.connection.format(query, variables);
+            console.log('Formatted Query:', formattedQuery);
+            Database.connection.query(formattedQuery, (error, result) => {
                 if (error) {
-                    reject(error);
+                    reject(new DatabaseError(error));
                 } else {
-                    resolve(results);
+                    console.log(result);
+                    resolve(result);
                 }
+            });
+        });
+    }
+
+    public static async insert(record: BaseModel): Promise<mysql.OkPacket> {
+        return new Promise<mysql.OkPacket>((resolve, reject) => {
+            Database.connection.beginTransaction(async (transactionError: mysql.MysqlError) => {
+                if (transactionError) {
+                    return reject(new DatabaseError(transactionError));
+                }
+                const table = record.constructor.name;
+                const recordClone = await record.createQuerySafeClone();
+                const query = mysql.format('INSERT INTO ?? SET ?;', [table, recordClone]);
+                console.log(query);
+                Database.connection.query(query, (insertError: mysql.MysqlError, result: mysql.OkPacket) => {
+                    if (insertError) {
+                        Database.connection.rollback(() => reject(new DatabaseError(insertError)));
+                    } else {
+                        Database.connection.commit((commitError: mysql.MysqlError) => {
+                            if (commitError) {
+                                reject(new DatabaseError(commitError));
+                            } else {
+                                resolve(result);
+                            }
+                        });
+                    }
+                });
+            });
+        });
+    }
+
+    public static async update(record: BaseModel): Promise<mysql.OkPacket> {
+        return new Promise<mysql.OkPacket>((resolve, reject) => {
+            if (!record?.Id) {
+                return reject(new DatabaseError(Constants.ERROR_MESSAGES.CANNOT_UPDATE_RECORD_WITHOUT_ID));
+            }
+            Database.connection.beginTransaction(async (transactionError: mysql.MysqlError) => {
+                if (transactionError) {
+                    return reject(new DatabaseError(transactionError));
+                }
+                const table = record.constructor.name;
+                const recordClone = await record.createQuerySafeClone();
+                const query = mysql.format('UPDATE ?? SET ? WHERE Id = ?', [table, recordClone, record.Id]);
+                console.log(query);
+                Database.connection.query(query, (updateError: mysql.MysqlError, result: mysql.OkPacket) => {
+                    if (updateError) {
+                        Database.connection.rollback(() => reject(new DatabaseError(updateError)));
+                    } else {
+                        Database.connection.commit((commitError: mysql.MysqlError) => {
+                            if (commitError) {
+                                reject(new DatabaseError(commitError));
+                            } else {
+                                console.log('UPDATE RESULT:', result);
+                                resolve(result);
+                            }
+                        });
+                    }
+                });
+            });
+        });
+    }
+
+    public static async delete(record: BaseModel | string): Promise<mysql.OkPacket> {
+        return new Promise<mysql.OkPacket>((resolve, reject) => {
+            const recordId = record instanceof BaseModel ? record.Id : record;
+            if (!recordId?.length) {
+                return reject(new DatabaseError(Constants.ERROR_MESSAGES.CANNOT_DELETE_RECORD_WITHOUT_ID));
+            }
+            Database.connection.beginTransaction(async (transactionError) => {
+                if (transactionError) {
+                    return reject(new DatabaseError(transactionError));
+                }
+                const table = record.constructor.name;
+                const id = recordId;
+                const query = mysql.format('DELETE FROM ?? WHERE Id = ?;', [table, id]);
+                console.log(query);
+                Database.connection.query(query, (deleteError: mysql.MysqlError, result: mysql.OkPacket) => {
+                    if (deleteError) {
+                        Database.connection.rollback(() => reject(new DatabaseError(deleteError)));
+                    } else {
+                        Database.connection.commit((commitError: mysql.MysqlError) => {
+                            if (commitError) {
+                                reject(new DatabaseError(commitError));
+                            } else {
+                                console.log('Successfully deleted', record, result);
+                                resolve(result);
+                            }
+                        });
+                    }
+                });
             });
         });
     }
--- src/database/user-repository.ts
+++ src/database/user-repository.ts
@@ -1,9 +1,130 @@
+import { DatabaseError, RequestError } from '@models/errors';
+import Database from '@database/database';
+import Constants from '@constants';
 import User from '@models/user';
+import Crypto from '@security/crypto';
+import StringUtils from '@utils/string';
+
+const CLIENT_VISIBLE_FIELDS = ['FirstName', 'LastName', 'Email', 'Phone', 'CreatedDate', 'EmailVerified', 'IsActive', 'LastModifiedDate', 'ActivatedDate'];
+
+const CREATEABLE_FIELDS = ['FirstName', 'LastName', 'Email', 'Phone', 'Password'];
+const REQUIRED_CREATE_FIELDS = ['Email', 'Password'];
+
+const UPDATEABLE_FIELDS = ['FirstName', 'LastName', 'Email', 'Phone'];
 
 export default class UserRepository {
-    public static getUserForAuthentication(email: string): User {
-        const user = new User();
-        user.Email = email;
+    public static async getUserForAuthentication(email: string): Promise<User> {
+        email = Database.connection.escape(email);
+        const userRecords = await Database.query(`SELECT Id, Email, Password FROM User WHERE Email = '${email}' LIMIT 1;`);
+        if (!userRecords.length) {
+            const errorMessage = StringUtils.format(Constants.ERROR_MESSAGES.USER_NOT_FOUND, ['email']);
+            throw new RequestError(Constants.ERROR_CODES.NOT_FOUND, errorMessage);
+        }
+        const user = await User.from(userRecords[0]);
+        console.log(user);
+        return user;
+    }
+
+    public static async getUserDetails(id: string): Promise<User> {
+        const userRecords = await Database.query(`SELECT ${CLIENT_VISIBLE_FIELDS.join(',')} FROM User WHERE Id = ? LIMIT 1;`, [id]);
+        if (!userRecords.length) {
+            const errorMessage = StringUtils.format(Constants.ERROR_MESSAGES.USER_NOT_FOUND, ['id']);
+            throw new RequestError(Constants.ERROR_CODES.NOT_FOUND, errorMessage);
+        }
+        console.log('User Record:', userRecords);
+        const user = await User.from(userRecords[0]);
+        console.log(user);
+        return user;
+    }
+
+    public static async createUser(user: User): Promise<User> {
+        // Ensure all provided fields are createable and all required fields are present
+        const invalidFields = [];
+        const requiredFields = [...REQUIRED_CREATE_FIELDS];
+        for (const field of Object.keys(user)) {
+            if (!CREATEABLE_FIELDS.includes(field)) {
+                invalidFields.push(field);
+            }
+            const requiredFieldIndex = requiredFields.indexOf(field);
+            if (requiredFieldIndex >= 0) {
+                requiredFields.splice(requiredFieldIndex, 1);
+            }
+        }
+        if (invalidFields.length) {
+            const pluralStr = invalidFields.length > 1 ? 's' : '';
+            const errorMessage = StringUtils.format(Constants.ERROR_MESSAGES.INVALID_FIELDS, [pluralStr, invalidFields.join(', ')]);
+            throw new RequestError(Constants.ERROR_CODES.BAD_REQUEST, errorMessage);
+        }
+        if (requiredFields.length) {
+            const pluralStr = requiredFields.length > 1 ? 's' : '';
+            const errorMessage = StringUtils.format(Constants.ERROR_MESSAGES.REQUIRED_FIELDS_MISSING, [pluralStr, requiredFields.join(', ')]);
+            throw new RequestError(Constants.ERROR_CODES.BAD_REQUEST, errorMessage);
+        }
+        try {
+            user.Password = await Crypto.hashPassword(user.Password!);
+
+            // Insert the user record, apply the new record id, and clear out the password before returning the new user record
+            const result = await Database.insert(user);
+
+            user.Id = result.insertId.toString();
+            user.Password = undefined;
+
+            return user;
+        } catch (error) {
+            if (error instanceof DatabaseError) {
+                // Override default MySQL error messages with more user-friendly custom error messages
+                switch (error.code) {
+                    case Constants.MYSQL_ERROR_CODES.ER_DUP_ENTRY:
+                        error.message = Constants.ERROR_MESSAGES.USER_ALREADY_EXISTS;
+                    default:
+                        break;
+                }
+            }
+            throw error;
+        }
+    }
+
+    public static async updateUser(user: User): Promise<User> {
+        const invalidFields = [];
+        for (const field of Object.keys(user).filter((f) => f !== 'Id')) {
+            if (!UPDATEABLE_FIELDS.includes(field)) {
+                invalidFields.push(field);
+            }
+        }
+        if (invalidFields.length) {
+            const pluralStr = invalidFields.length > 1 ? 's' : '';
+            const errorMessage = StringUtils.format(Constants.ERROR_MESSAGES.INVALID_FIELDS, [pluralStr, invalidFields.join(', ')]);
+            throw new RequestError(Constants.ERROR_CODES.BAD_REQUEST, errorMessage);
+        }
+
+        const result = await Database.update(user);
+        if (result.affectedRows === 0) {
+            const errorMessage = StringUtils.format(Constants.ERROR_MESSAGES.USER_NOT_FOUND, ['id']);
+            throw new RequestError(Constants.ERROR_CODES.NOT_FOUND, errorMessage);
+        }
+        return user;
+    }
+
+    public static async deleteUser(user: User | string): Promise<User> {
+        if (typeof user === 'string') {
+            const userId = user;
+            user = await User.from({ Id: userId });
+        }
+        const result = await Database.delete(user);
+        console.log('DELETE RESULT:', result);
+        return user;
+    }
+
+    public static async deactivateUser(user: User | string): Promise<User> {
+        if (typeof user === 'string') {
+            const userId = user;
+            user = await User.from({ Id: userId, IsActive: false });
+        }
+        const result = await Database.update(user);
+        if (result.affectedRows === 0) {
+            const errorMessage = StringUtils.format(Constants.ERROR_MESSAGES.USER_NOT_FOUND, ['id']);
+            throw new RequestError(Constants.ERROR_CODES.NOT_FOUND, errorMessage);
+        }
         return user;
     }
 }
